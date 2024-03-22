18eb59463a8692496e73f029c6e6af4c34675590
Gan Yang <gabriel.yang@mail.utoronto.ca>
2024-03-22 00:05:35 -0400

    Refactor aes-encrypt-openssl.c and add utils.

--- aes-encrypt-openssl.c
+++ aes-encrypt-openssl.c
@@ -1,60 +1,12 @@
 // To build and run:
-// nvcc aes-encrypt-openssl.c -o aes-encrypt-openssl -lcrypto -lssl && ./aes-encrypt-openssl
+// nvcc aes-encrypt-openssl.c utils.c -o aes-encrypt-openssl -lcrypto -lssl && ./aes-encrypt-openssl
 
+#include "utils.h"
 #include <openssl/evp.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define AES_KEY_SIZE 16
 #define AES_BLOCK_SIZE 16
-
-// Function to read key or IV from a file
-void read_key_or_iv(unsigned char *data, size_t size, const char *filename) {
-  FILE *file = fopen(filename, "r");
-  if (file == NULL) {
-    fprintf(stderr, "Cannot open file: %s\n", filename);
-    exit(1);
-  }
-  for (size_t i = 0; i < size; i++) {
-    char buffer[3];
-    if (fread(buffer, 1, 2, file) != 2) {
-      fprintf(stderr, "Cannot read value from file: %s\n", filename);
-      exit(1);
-    }
-    buffer[2] = '\0'; // Null-terminate the buffer
-    data[i] = (unsigned char)strtol(
-        buffer, NULL, 16); // Convert the buffer to a hexadecimal value
-  }
-  fclose(file);
-}
-
-// Function to read plaintext from a file
-void read_plaintext(unsigned char *plaintext, size_t size,
-                    const char *filename) {
-  FILE *file = fopen(filename, "r");
-  if (file == NULL) {
-    fprintf(stderr, "Cannot open file: %s\n", filename);
-    exit(1);
-  }
-  fgets((char *)plaintext, size, file);
-  fclose(file);
-}
-
-// Function to write ciphertext to a file
-void write_ciphertext(const unsigned char *ciphertext, size_t size,
-                      const char *filename) {
-  FILE *file = fopen(filename, "w");
-  if (file == NULL) {
-    fprintf(stderr, "Cannot open file: %s\n", filename);
-    exit(1);
-  }
-  for (size_t i = 0; i < size; i++) {
-    fprintf(file, "%02x", ciphertext[i]);
-  }
-  fprintf(file, "\n");
-  fclose(file);
-}
 
 int aes_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
                 unsigned char *ivec, unsigned char *ciphertext) {
