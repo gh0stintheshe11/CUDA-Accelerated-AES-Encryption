e6927d2aa71d9b31ccd4b4d31eb1a08dd09fa6c9
gh0stintheshe11 <langs.971104@gmail.com>
2024-04-05 20:24:18 -0400

    update openssl decryption script to match the encrypt program and handle the file extension

--- openssl-dec.sh
+++ openssl-dec.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# openssl-dec.sh: Decrypt data file with openssl. Make sure input is
-# in binary. (not ascii encoded binaray...) Stolen from openssl-check.sh
-
+# Check if input, key, IV files, and output file name are provided
 if [ "$#" -ne 4 ]; then
-	echo "Usage: $0 <data file> <key file> <IV file> <output file>"
-	exit 1
+    echo "Usage: $0 <encrypted file> <key file> <IV file> <output file>"
+    exit 1
 fi
 
-data_file=$1
+# Assign arguments to variables
+encrypted_file=$1
 key_file=$2
 iv_file=$3
 output_file=$4
 
+# Read key and IV from files, removing any newlines
 key=$(tr -d '\n' < "$key_file")
 iv=$(tr -d '\n' < "$iv_file")
 
-openssl enc -aes-128-ctr -d -in "$data_file" -out "$output_file" -K "$key" -iv "$iv" -nosalt
-echo "result written in $output_file"
+# Read the file extension from the end of the encrypted file
+extension=$(strings "$encrypted_file" | grep -oE "\.[a-zA-Z0-9]*$" | tail -n 1)
+
+# Perform the decryption
+openssl enc -aes-128-ctr -d -in "$encrypted_file" -out "$output_file$extension" -K "$key" -iv "$iv" -nosalt
+echo "Decryption complete. Decrypted file written in $output_file$extension"
